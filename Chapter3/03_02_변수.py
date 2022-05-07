0b2ce6a5d4c0638a0f3f95a5c02eeea17ea85424
irumdev <jklsj1252@gmail.com>
2022-05-07 13:17:02 +0900

    Chapter3. 자료형과 변수
    03_01 숫자, 문자, 불린
    03_02 변수

---
+++ Chapter3/03_02_변수.py
@@ -0,0 +1,22 @@
+# 변수 값 초기화
+name = "콜라"
+calorie = 300
+volume = 320
+unit = "ml"
+print(name, calorie, volume, unit)
+
+
+# 변수 값 변경
+name = "사이다"
+calorie = 680
+volume = 1
+unit = "l"
+print(name, calorie, volume, unit)
+
+
+# Indirection
+name = "칠성" + name
+calorie = calorie + 100
+volume = volume + 1
+unit = "l"
+print(name, calorie, volume, unit)
